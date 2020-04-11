0ef59aa8f388bb1b312349ae929a3e34dccaa6c2
grpartichelli <gabrielpartichelli@gmail.com>
2020-04-11 13:26:23 -0300

    Separate keys, update auth flow

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("./config/keys");
+
+require("./models/User");
+require("./services/passport"); //runs the code inside passport
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleCLientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    acessToken => {
-      console.log(acessToken);
-    }
-  )
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
 );
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app); //passes app as a paremeter of the function that the require returns
+
+mongoose.connect(keys.mongoURI); //connects to the database
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ routes/authRoutes.js
@@ -0,0 +1,20 @@
+const passport = require("passport"); //original passport module
+
+//this arrow function allows this file to access the app
+module.exports = (app) => {
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["profile", "email"] })
+  );
+
+  app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
+};
