9bd5af1a08994bb787ea6b0983329c2b0eff038b
grpartichelli <gabrielpartichelli@gmail.com>
2020-04-11 14:17:47 -0300

    test

--- services/passport.js
+++ services/passport.js
@@ -21,6 +21,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleCLientSecret,
       callbackURL: "/auth/google/callback",
+      proxy: true,
     },
     (acessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then((existingUser) => {
