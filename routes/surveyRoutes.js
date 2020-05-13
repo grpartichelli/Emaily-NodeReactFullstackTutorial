23d73cbbc7b3a38d89eac3e7f9d0a16fcac8829f
grpartichelli <gabrielpartichelli@gmail.com>
2020-05-13 13:37:49 -0300

    Update dashboard to display survey info

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -72,4 +72,11 @@ module.exports = (app) => {
   app.get("/api/surveys/:surveyId/:choice", (req, res) => {
     res.send("Thanks for voting!");
   });
+
+  app.get("/api/surveys", requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false,
+    });
+    res.send(surveys);
+  });
 };
